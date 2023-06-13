0c1620ecb39f3b15f973298664593bf902681ab4
Benjamin <bmckee566@gmail.com>
2023-06-13 16:52:12 -0400

    Task finish

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        System.out.println("Enter your number of the month you were born: ");
+        int birthMonth = in.nextInt();
+
+        if(birthMonth >= 1 && birthMonth <= 12) {
+            System.out.println("Your birth month is: " + birthMonth);
+        } else {
+            System.out.println("Invalid birth month.");
+        }
     }
 }
